c6c602ed54baca5917dc45575e343608b54c3d28
KatherineHanson <katherinemariehanson@gmail.com>
2017-10-19 23:27:41 -0700

    trimmed code again

--- lab-katherine/src/main.js
+++ lab-katherine/src/main.js
@@ -3,8 +3,6 @@ import './style/main.scss'
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './component/app'
-import 'core-js/es6/map'
-import 'core-js/es6/set'
 
 const container = document.createElement('div')
 document.body.appendChild(container)
