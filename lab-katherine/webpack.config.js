f3c9fb5daceb134ac4f5502918ecad7b0b14628d
KatherineHanson <katherinemariehanson@gmail.com>
2017-10-19 20:43:37 -0700

    added scss styling

--- lab-katherine/webpack.config.js
+++ lab-katherine/webpack.config.js
@@ -1,24 +1,41 @@
-'use strict'
-const HTMLPlugin = require('html-webpack-plugin')
+'use strict';
+
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: `${__dirname}/src/main.js`,
-  devtool: 'cheap-eval-source-map',
-  devServer: {
-    historyApiFallback: true,
-  },
-  output: {
+  output:  {
     path: `${__dirname}/build`,
     filename: 'bundle.[hash].js',
   },
-  plugins: [new HTMLPlugin()],
+  devtool: 'cheap-eval-source-map',
+  plugins: [
+    new HTMLPlugin(),
+    new ExtractPlugin('bundle.[hash].css'),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractPlugin.extract({
+          use: [
+            'css-loader',
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
       },
     ],
   },
-}
+};
